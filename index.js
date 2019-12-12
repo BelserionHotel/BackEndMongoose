c5edc88c50fe8cdc7f7d7f4d5335a8f49f0c54c6
Amirullah23 <amirullah.kasang@gmail.com>
2019-12-12 18:27:30 +0700

    add token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require("express-jwt")
 const app = express()
 const bodyParser = require("body-parser")
 const {PORT, db} = require("./config")
@@ -7,6 +8,27 @@ const {PORT, db} = require("./config")
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use(jwt(
+    {secret : "amir"}).unless({
+        path: [
+            {
+                url: "/",
+                methods: ["GET"]
+            }
+        ]
+    })
+)
+app.use((err, req, res, next) => {
+    console.log(err);
+    
+        if (err.name === "UnauthorizedError") {
+            return res.status(401).json({
+                message: "You are not allow to enter this endpoints"
+            });
+        }
+        return next();
+    });
 
 app.use("/", require("./routes"));
 app.use("/users", require("./routes/users"));
