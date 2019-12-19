0a103271f1573f47c3f91363401396ce088ce212
ahmdkla <ahmadakla48@gmail.com>
2019-12-19 14:19:55 +0700

    modify models of reservation, checkout to valid

--- models/roles.js
+++ models/roles.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const rolesSchema = new Schema(
-  {
-    Name: {
-      type: String,
-      required: true
-    }
-  }
+    {
+        Name: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
 );
 
 const Roles = mongoose.model("roles", rolesSchema);
