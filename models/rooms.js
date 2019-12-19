68a5ad1db02a4c6413502f994cb292f03bc79ca0
ahmdkla <ahmadakla48@gmail.com>
2019-12-19 14:42:15 +0700

    modify models room default 'available'

--- models/rooms.js
+++ models/rooms.js
@@ -2,23 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const roomSchema = new Schema(
-  {
-    RoomType_id: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "roomTypes"
+    {
+        RoomType_id: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "roomTypes"
+        },
+        RoomNumber: {
+            type: String,
+            required: true
+        },
+        RoomFloor: {
+            type: String,
+            required: true
+        },
+        availability: { type: String, default: "Available" }
     },
-    RoomNumber: {
-      type: String,
-      required: true
-    },
-    RoomFloor: {
-      type: String,
-      required: true
-    },
-    availability: String
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Rooms = mongoose.model("rooms", roomSchema);
